2a8d364f18de221d31416c1ad3e56fb132f9ad73
Wesley <wguthrie@robofirm.com>
2017-06-23 02:07:06 -0400

    add dependency injection container

--- src/Application.php
+++ src/Application.php
@@ -2,7 +2,27 @@
 namespace Modulus;
 
 use Symfony\Component\Console\Application as ConsoleApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class Application extends ConsoleApplication {
+class Application extends ConsoleApplication
+{
+    /** @var ContainerBuilder $container */
+    private $container;
 
+    public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN')
+    {
+        $this->container = new ContainerBuilder();
+
+        //$loader = new XmlFileLoader($this->container, new FileLocator(__DIR__.'/../../config'));
+        //$loader->load('services.xml');
+
+        parent::__construct($name, $version);
+    }
+
+    public function run(InputInterface $input = null, OutputInterface $output = null)
+    {
+        parent::run($input,$output);
+    }
 }
