70e3dd840d93dec9779ac7d2a0ee55f68f202b52
Wesley <wguthrie@robofirm.com>
2017-06-24 01:34:56 -0400

    add module loading directories

--- src/Application.php
+++ src/Application.php
@@ -11,18 +11,37 @@ class Application extends ConsoleApplication
     /** @var ContainerBuilder $container */
     private $container;
 
+    private $localModuleDir;
+    private $composerModuleDir;
+
     public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN')
     {
         $this->container = new ContainerBuilder();
+        $this->localModuleDir = getenv("HOME") . '/.modulus/modules/';
+        $this->composerModuleDir = __DIR__;
 
-        //$loader = new XmlFileLoader($this->container, new FileLocator(__DIR__.'/../../config'));
-        //$loader->load('services.xml');
+        $this->loadLocalModules();
+        $this->loadComposerModules();
 
         parent::__construct($name, $version);
     }
 
     public function run(InputInterface $input = null, OutputInterface $output = null)
     {
+        echo $this->localModuleDir . "\n";
+        echo $this->composerModuleDir;
         parent::run($input,$output);
+    }
+
+    private function loadLocalModules()
+    {
+
+        //$loader = new XmlFileLoader($this->container, new FileLocator(__DIR__.'/../../config'));
+        //$loader->load('services.xml');
+    }
+
+    private function loadComposerModules()
+    {
+
     }
 }
